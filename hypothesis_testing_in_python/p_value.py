bb8def4394182d4319362acb3490053476bf9927
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-02-13 07:17:31 +0000

    Update 1_3

--- hypothesis_testing_in_python/p_value.py
+++ hypothesis_testing_in_python/p_value.py
@@ -1,3 +1,13 @@
 #Calculating p-values
 #to determine whether to choose the null hypothesis or the alternative hypothesis, you need to calculate a p-value from the z-score.
 
+#The null hypothesis, is that the proportion of late shipments is six percent.
+#alternative hypothesis, is that the proportion of late shipments is greater than six percent.
+# Calculate the z-score of late_prop_samp
+z_score = (late_prop_samp - late_prop_hyp) / std_error
+
+# Calculate the p-value
+p_value = 1 - norm.cdf(z_score)
+                 
+# Print the p-value
+print(p_value)
