69e2f650823280e716c5b04edd2b3dfdac504b80
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-02-18 07:51:49 +0000

    2_3

---
+++ Introduction_to_statistics_in_python/probability distribution.py
@@ -0,0 +1,15 @@
+
+#creating list in py 1 to 6
+print(list(range(1,7)))
+#random dice casting
+random_dice = np.random.randint(1,7)
+print("Dice casted:",random_dice)
+
+#np.random.seed(1)
+print(df.sample())
+#sampling without replacement
+
+# Create a histogram of restaurant_groups and show plot
+restaurant_groups['group_size'].hist(bins=[2, 3, 4, 5, 6])
+plt.show()
+
