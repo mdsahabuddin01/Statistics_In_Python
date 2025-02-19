29c4c8cce2362137de08c5dadc589c14facb82b1
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2025-02-19 13:31:06 +0000

    2_3 v2

--- Introduction_to_statistics_in_python/probability distribution.py
+++ Introduction_to_statistics_in_python/probability distribution.py
@@ -1,3 +1,13 @@
+"""
+Create a histogram of the group_size column of restaurant_groups, setting bins to [2, 3, 4, 5, 6]. Remember to show the plot.
+Count the number of each group_size in restaurant_groups, then divide by the number of rows in restaurant_groups to calculate the probability of randomly selecting a group of each size. Save as size_dist.
+Reset the index of size_dist.
+Rename the columns of size_dist to group_size and prob.
+Calculate the expected value of the size_dist, which represents the expected group size, by multiplying the group_size by the prob and taking the sum.
+4
+Calculate the probability of randomly picking a group of 4 or more people by subsetting for groups of size 4 or more and summing the probabilities of selecting those groups.
+
+"""
 
 #creating list in py 1 to 6
 print(list(range(1,7)))
@@ -12,4 +22,21 @@ print(df.sample())
 # Create a histogram of restaurant_groups and show plot
 restaurant_groups['group_size'].hist(bins=[2, 3, 4, 5, 6])
 plt.show()
+
+# Create probability distribution
+size_dist = restaurant_groups['group_size'].value_counts() / restaurant_groups.shape[0]
+# Reset index and rename columns
+size_dist = size_dist.reset_index()
+size_dist.columns = ['group_size', 'prob']
+
+# Expected value
+expected_value = np.sum(size_dist['group_size'] * size_dist['prob'])
+
+# Subset groups of size 4 or more
+groups_4_or_more = size_dist[size_dist['group_size'] >= 4]
+
+# Sum the probabilities of groups_4_or_more
+prob_4_or_more = np.sum(groups_4_or_more['prob'])
+print(prob_4_or_more)
+
 
